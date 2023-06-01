b92a3c00865b1b935ed654339e19f3fe4c9684b5
rpwysocki <134246531+rpwysocki@users.noreply.github.com>
2023-06-01 14:34:15 -0400

    testing connection

--- js/script.js
+++ js/script.js
@@ -1,40 +1,78 @@
-// var students = ['John', 'FSF', 'Jamil', 'FSF', 'Roman', 'FSF'];
-
-
-
-
-// for (var index = 0; index < students.length; index++) {
-//     console.log('Nmae:', students[index])
-//     console.log('Course:', students[index + 1])
-// }
-
-// function printStudentInfo() { 
-//         console.log('Nmae:', students[index])
-//         console.log('Course:', students[index + 1])
-//}
 
 var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', ':', ';', '<', '>', '.', '?', '/'];
 
+var combined = [];
 
-// var random = Math.random();
-        var combined = lowercase.concat(uppercase).concat(numbers).concat(specialCharacters);
-        var amount = 50;
+
+
+// parseInt converts a string into a number, + also works in place of number
+
+//Confirm specials and numbers as well
+
+
+
+
+function responses(){
+        var amount = Number(prompt('Please provide an amount characters.'));
+        var includeLowercase = confirm ('Click OK to include lowercase letters');
+        var includeUppercase = confirm ('Click OK to include uppercase letters');
+        var includeNumbers = confirm ('Click OK to include lowercase numbers');
+        var includeSpecial = confirm ('Click OK to include special characters');
+        console.log(amount, includeLowercase, includeUppercase, includeNumbers, includeSpecial);
+        return{amount, includeLowercase, includeUppercase, includeNumbers, includeSpecial};
+}
+
+var selections = [responses()];
+console.log(selections);
+
+var choices = getUserInput();
+
+
+
+function generatePass() {
+        var combinedChars = [];
         var password = '';
-        // var randomIndex = 10;
-// function randomLowercaseLetters(amount) {
 
+        var includeLowercase = choices[1]
+        var includeUppercase = choices[2]
+        var includeNumbers = choices[3]
+        var includeSpecial = choices[4]
+
+
+        for (var index = 0; index < amount; index ++ ){
+
+        }
+        
+}
+
+
+if (includeLowercase) {
+        combined=combined.concat(lowercase)
+}
+
+if (includeUppercase)  {
+        combined=combined.concat(uppercase)
+}
+
+if (includeNumbers)  {
+        combined=combined.concat(numbers)
+}
+
+if (includeSpecial)  {
+        combined=combined.concat(specialCharacters)
+}
+
+console.log
+
+
+alert('Thank you for your input')
+
+// function sum(a, b){
+//         return a + b;
 // }
-//   var lowercaseArray = 'abcdefghijklmnopqrstuvwxyz';
 
-
-for (var count = 0; index < amount; count++)
-        var randomNum = Math.random()
-        var ranIndex = Math.floor(Math.random() * combined.length);
-        password += combined[randomIndex]
-      
-console.log(password);
-
-document.body.innerText = password;
+// var result = sum(5, 10);
+// console.log(result);
